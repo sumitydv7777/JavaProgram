38ba3dc81a792f4491edf02db0e61b481dddf416
sumitydv7777 <sumitydv7777@gmail.com>
2022-12-30 17:05:54 +0530

    First commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+package D_2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter no rows");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
