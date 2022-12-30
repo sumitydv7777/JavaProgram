5041f85351bc027690221e22361a73a936e6d72f
sumitydv7777 <121486640+sumitydv7777@users.noreply.github.com>
2022-12-30 23:02:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package D_2;
 
 import java.util.Scanner;
 
-public class Main {
+public class alpha {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter no rows");
